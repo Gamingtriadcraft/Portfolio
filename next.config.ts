12c5c20d5a0dad929ec8fb631d4da80e450479d2
thedogecraft <dantaealt@gmail.com>
2026-01-26 21:02:36 -0700

    Revert "fix: next"
    This reverts commit bbd833e58cdb90203025d6e66c582d675df4fabc.

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true, // needed if you’re using <Image>
-  },
+  /* config options here */
 };
 
 export default nextConfig;
